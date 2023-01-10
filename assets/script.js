89e5550d8bb64ee060f68b65cfd769ed66e6108d
Abbi <abbi.r.hickey@hotmail.co.uk>
2023-01-10 13:15:33 +0000

    added loop to generate random password from large character array

--- assets/script.js
+++ assets/script.js
@@ -92,7 +92,7 @@ var upperCasedCharacters = [
 function getPasswordOptions() {
 
   // length prompt 
-  let passwordLength = prompt("How many characters generated? Min 10 : Max 64")
+  let passwordLength = prompt("How many characters generated? Please state number between 10 and 64")
     // Using an if statements to remind user of length requirements 
   if (passwordLength < 10) {
     alert("Password must be at least 10 characters");
@@ -101,32 +101,59 @@ function getPasswordOptions() {
     alert("password must not exceed 64 characters");
   }
 }
-  // Checking for different character types 
+
+
+// Checking for different character types 
   //Include lowercase 
-  let passLowCase = confirm("Include lower case?");
+  let LowCase = confirm("Include lower case?");
 
   //Include uppercase 
-  let passUpCase = confirm("Include upper case?");
+  let UpCase = confirm("Include upper case?");
 
   //Include numbers
   let passNum = confirm("Include numbers?");
 
   // Include special characters
-  let passSpecChar = confirm("Include special characters?");
+  let SpecChar = confirm("Include special characters?");
 
   //prompt if no character type included. 
-  if (passLowcase === false && passupcase === false && passnum === false && passspecchar === false) { 
+  if (Lowcase === false && passupcase === false && passnum === false && passspecchar === false) { 
     alert("Must include at least one character type")
   }
 
+ // Store password options in object. 
+let userPassOptions = { 
+  length: passwordLength,
+  lowercase: LowCase,
+  uppercase: UpCase,
+  numbers: passNum,
+  specialCharacters: SpecChar,
+}
 
+// Link password options to variable character arrays listed at top of document to create an array of the users choice. 
+if (userPasswordOptions.specialCharacters === true) {
+  UserPassArray = UserPassArray.concat(specialCharacters);
+}
+if (userPasswordOptions.containsNumbers === true) {
+  UserPassArray = UserPassArray.concat(numericCharacters);
+}
+if (userPasswordOptions.lowercase === true) {
+  UserPassArray = UserPassArray.concat(lowerCasedCharacters);
+}
+if (userPasswordOptions.uppercase === true) {
+  UserPassArray = UserPassArray.concat(upperCasedCharacters);
+}
+console.log(UserPassArray);
 
 
 // Function for getting a random element from an array
 function getRandom(arr) {
 
 }
+  // Using a for loop math random to generate an array from the password options. 
+  var randompass = Math.floor(Math.random() * UserPassArray.length)
 
+console.log(randompass) 
 // Function to generate password with user input
 function generatePassword() {
 
